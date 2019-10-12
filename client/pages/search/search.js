e8c1e6fca7f6e6e034f2279d6a9d4a5362fa42b4
lurongtao <lurongtao@126.com>
2019-10-12 10:03:45 +0800

    2019-10-12-search-2

--- client/pages/search/search.js
+++ client/pages/search/search.js
@@ -1,38 +1,70 @@
 Page({
   onLoad() {
     this.setData({
-      handleSearch: this.handleSearch.bind(this)
+      // handleSearch: this.handleSearch.bind(this)
+      // keyword: ''
+      results: []
     })
   },
 
-  handleSearch(keyword) {
-    return new Promise((resolve, reject) => {
-      wx.request({
-        url: 'https://ik9hkddr.qcloud.la/index.php/trade/get_search_list',
-        method: 'post',
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-          keyword
-        },
-        success(ret) {
-          let result = ret.data.data
-          let arr = result.map((item) => {
-            return {
-              text: item.message,
-              value: item.id
-            }
-          })
-          resolve(arr)
-        }
-      })
-    })
+  // handleSearch(keyword) {
+  //   return new Promise((resolve, reject) => {
+  //     wx.request({
+  //       url: 'https://ik9hkddr.qcloud.la/index.php/trade/get_search_list',
+  //       method: 'post',
+  //       header: {
+  //         'content-type': 'application/x-www-form-urlencoded'
+  //       },
+  //       data: {
+  //         keyword
+  //       },
+  //       success(ret) {
+  //         let result = ret.data.data
+  //         let arr = result.map((item) => {
+  //           return {
+  //             text: item.message,
+  //             value: item.id
+  //           }
+  //         })
+  //         resolve(arr)
+  //       }
+  //     })
+  //   })
+  // },
+
+  // selectResult(e) {
+  //   wx.navigateTo({
+  //     url: '/pages/detail/detail?id=' + e.detail.item.value,
+  //   })
+  // }
+
+  handleBlur(e) {
+    this.doSearch(e.detail.value)
   },
 
-  selectResult(e) {
-    wx.navigateTo({
-      url: '/pages/detail/detail?id=' + e.detail.item.value,
+  doSearch(keyword) {
+    wx.request({
+      url: 'https://ik9hkddr.qcloud.la/index.php/trade/get_search_list',
+      method: 'post',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        keyword
+      },
+      success: (ret) => {
+        this.setData({
+          results: ret.data.data
+        })
+        // let result = ret.data.data
+        // let arr = result.map((item) => {
+        //   return {
+        //     text: item.message,
+        //     value: item.id
+        //   }
+        // })
+        // resolve(arr)
+      }
     })
   }
 })
